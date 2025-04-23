9a401b9f7ccb9e64a34c22dbb04ace663218ec5d
Leora Spinner <leoraspinner@Leoras-MacBook-Air-2.local>
2025-04-23 12:07:28 -0400

    took out comment

--- src/main/java/spinner/paint/Tool.java
+++ src/main/java/spinner/paint/Tool.java
@@ -6,9 +6,7 @@ public interface Tool
 {
     void pressed(Graphics g, int x, int y);
     void dragged(Graphics g, int x, int y);
-    /*
-     * Draws a preview of the tool's operation if released is called
-     * */
+
     void preview(Graphics g);
     void released(Graphics g, int x, int y);
 
